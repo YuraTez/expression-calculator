222dc64c7820e5cee60791efdf8220a4661d0bf3
YuraTez <52445676+YuraTez@users.noreply.github.com>
2020-03-22 23:09:16 +0300

    fiat: part of the decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 function eval() {
-    // Do not use eval!!!
+
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr=expr.replace(/\s+/g,'');
+
+    let valueZero = expr.match(/\d*\/0/);
+    if (expr.split("(").length !== expr.split(")").length ) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+    let som = valueZero;
+    if(som){
+        throw new Error ("TypeError: Division by zero.");
+    }
+
+    let value = expr.match(/(^[0-9*/+-]+$)/);
+    let sum = new Function('return '+ value );
+
+    return sum();
 }
 
 module.exports = {
     expressionCalculator
-}
+};
+
